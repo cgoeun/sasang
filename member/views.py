01ab4984f14db4fbd051dd3d477124eccdf54987
cgoeun <innn900002@gmail.com>
2022-07-21 11:16:34 +0900

    .
    합쳤슴돠

--- member/views.py
+++ member/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Member
+from django.utils import timezone
+from django.http import HttpResponse
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        user_pw = request.POST.get('user_pw')
+        user_name = request.POST.get('user_name')
+        user_birth = request.POST.get('user_birth')
+        user_gender = request.POST.get('user_gender')
+
+        m = Member(
+            user_id=user_id, user_pw=user_pw, user_name=user_name, user_birth=user_birth, user_gender=user_gender
+        )
+        m.date_joined = timezone.now()
+        m.save()
+        return HttpResponse(
+            '가입 완료<br>%s %s %s' % (user_id, user_pw, user_name, user_birth, user_gender))
+    else:
+      return render(request, 'member/signup_custom.html')
